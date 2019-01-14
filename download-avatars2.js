273175a6a06bcd8fcee64f216d48150e0c679681
ShahjamalMalik <shahjamalmalik@gmail.com>
2019-01-14 22:43:44 +0000

    Created a HTTPS client using request to download an image from the internet.

---
+++ download-avatars2.js
@@ -0,0 +1,14 @@
+var request = require('request');
+var fs = require('fs');
+
+request.get('https://sytantris.github.io/http-examples/future.jpg')               // Note 1
+       .on('error', function (err) {                                   // Note 2
+         throw err;
+       })
+       .on('response', function (response) {                           // Note 3
+         console.log('Response Status Code: ', response.statusCode);
+       })
+       .pipe(fs.createWriteStream('./future.jpg'));               // Note 4
+
+
+
