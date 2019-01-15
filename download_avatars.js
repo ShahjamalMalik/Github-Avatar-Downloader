b2347872508d5e5b8cbb0d3516f97b3279d1557e
ShahjamalMalik <shahjamalmalik@gmail.com>
2019-01-15 00:13:40 +0000

    Added a CB function that is able to console.log the avatar urls, added an authorization header, updated my .gitignore file

---
+++ download_avatars.js
@@ -0,0 +1,25 @@
+var request = require('request');
+var token = require('./secrets.js')
+console.log('Welcome to the GitHub Avatar Downloader!');
+
+function getRepoContributors(repoOwner, repoName, cb) {
+  var options = {
+    url: "https://api.github.com/repos/" + repoOwner + "/" + repoName + "/contributors",
+    headers: {
+      'User-Agent': 'request',
+      Authorization: 'Bearer ' + token.GITHUB_TOKEN
+    }
+  };
+
+  request(options, function(err, res, body) {
+    cb(err, JSON.parse(body));
+
+  });
+}
+
+getRepoContributors("jquery", "jquery", function(err, result) {
+  for (var i of result) {
+    console.log(i.avatar_url)
+  }
+});
+
